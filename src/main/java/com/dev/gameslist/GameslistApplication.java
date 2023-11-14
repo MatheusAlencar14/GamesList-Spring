3929488ebf7a10f1ac83d172749deaf05aa08333
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-11-14 09:06:46 -0300

    Criando projeto

---
+++ src/main/java/com/dev/gameslist/GameslistApplication.java
@@ -0,0 +1,14 @@
+package com.dev.gameslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameslistApplication.class, args);
+		System.out.printf("Deu Certo!");
+	}
+
+}
